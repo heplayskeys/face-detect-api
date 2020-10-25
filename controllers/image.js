02445b509f813469c8e9240c94f66e000d2ffd35
heplayskeys <heplayskeys@gmail.com>
2020-10-25 01:36:43 -0700

    Update api to v3

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,27 @@
 const Clarifai = require('clarifai');
-const API_KEY = process.env.API_CLARIFAI;
-const FACE_DETECT_MODEL = 'c0c0ac362b03416da06ab3fa36fb58e3';
+const fs = require('fs');
+const fileType = require('file-type');
+const multiparty = require('multiparty');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+const FACE_DETECT_MODEL = process.env.CLARIFAI_FACE_DETECT_MODEL;
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ID,
+	secretAccessKey: process.env.AWS_KEY
+});
+
+const s3 = new AWS.S3();
 
 const app = new Clarifai.App({
-	apiKey: API_KEY
+	apiKey: process.env.CLARIFAI_API_KEY
 });
 
 const handleClarifaiRequest = async (req, res) => {
 	try {
 		const request = await app.models.predict(FACE_DETECT_MODEL, req.body.input);
-		res.status(200).json(request);
+		res.json(request);
 	} catch {
 		res.status(400).json('Unable to complete request');
 	}
@@ -25,14 +37,74 @@ const updateUserEntries = db => async (req, res) => {
 				.returning('entries')
 				.where({ id })
 				.increment('entries', 1);
-			res.status(200).json(entries[0]);
+			res.json(entries[0]);
 		} catch {
 			res.status(404).json('Uh oh! Something went wrong.');
 		}
 	}
 };
 
+const loadFile = (buffer, name, type) => {
+	let params = {
+		ACL: 'public-read',
+		Bucket: process.env.AWS_BUCKET,
+		Body: buffer,
+		ContentType: type.mime,
+		Key: `${name}.${type.ext}`
+	};
+	return s3.upload(params).promise();
+};
+
+const uploadFile = (req, res) => {
+	const form = new multiparty.Form();
+	form.parse(req, async (error, fields, files) => {
+		if (error) {
+			return res.status(500).send(error);
+		}
+
+		try {
+			const path = files.file[0].path;
+			const buffer = fs.readFileSync(path);
+			const type = await fileType.fromBuffer(buffer);
+			const fileName = `${req.params.id}/${Date.now().toString()}`;
+			const data = await loadFile(buffer, fileName, type);
+			return res.status(200).send(data);
+		} catch (err) {
+			return res.status(500).send(err);
+		}
+	});
+};
+
+const emptyS3Folder = async (req, res) => {
+	const params = {
+		Bucket: process.env.AWS_BUCKET,
+		Prefix: `${req.params.id}/`
+	};
+
+	try {
+		const listedObjects = await s3.listObjectsV2(params).promise();
+
+		if (listedObjects.Contents.length === 0) return res.status(200);
+		deleteKeys = listedObjects.Contents.map(image => ({ Key: image.Key }));
+
+		const deleteParams = {
+			Bucket: process.env.AWS_BUCKET,
+			Delete: {
+				Objects: deleteKeys
+			}
+		};
+
+		await s3.deleteObjects(deleteParams).promise();
+
+		if (listedObjects.IsTruncated) await emptyS3Folder(req.params.id);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
 module.exports = {
 	handleClarifaiRequest,
-	updateUserEntries
+	updateUserEntries,
+	uploadFile,
+	emptyS3Folder
 };
--- controllers/register.js
+++ controllers/register.js
@@ -13,7 +13,7 @@ const registerUser = (db, bcrypt) => (req, res) => {
 		return res
 			.status(400)
 			.json(
-				'Password must be:\n  • Between 8 to 15 characters\n  • Contain at least one lowercase letter\n  • Contain at least one uppercase letter\n  • Contain at least one numeric digit\n  • Contain at least one special character'
+				'Password must contain:\n  • Between 8 and 15 characters\n  • At least 1 lowercase letter\n  • At least 1 uppercase letter\n  • At least 1 numeric digit\n  • At least 1 special character'
 			);
 	}
 
@@ -36,7 +36,7 @@ const registerUser = (db, bcrypt) => (req, res) => {
 						.into('users')
 						.returning('*');
 					await trx.commit();
-					res.status(200).json(user[0]);
+					res.json(user[0]);
 				} catch {
 					await trx.rollback();
 					res.status(400).json('Unable to register');
@@ -45,6 +45,56 @@ const registerUser = (db, bcrypt) => (req, res) => {
 		});
 	} else {
 		res.status(400).json('Passwords do not match');
+	}
+};
+
+const updateUserProfile = (db, bcrypt) => async (req, res) => {
+	if (req.body.name) {
+		const { id, name } = req.body;
+		const user = await db('users').select('*').where({ id });
+
+		if (user[0]) {
+			try {
+				await db('users').where({ id }).update({ name });
+				res.json(name);
+			} catch {
+				res.status(404).json('Uh oh! Something went wrong.');
+			}
+		}
+	} else {
+		const { id, oldPassword, newPassword } = req.body;
+
+		if (!validatePassword(newPassword)) {
+			return res
+				.status(400)
+				.json(
+					'Password must contain:\n  • Between 8 and 15 characters\n  • At least 1 lowercase letter\n  • At least 1 uppercase letter\n  • At least 1 numeric digit\n  • At least 1 special character'
+				);
+		}
+
+		const user = await db('users').select('*').where({ id });
+		const email = user[0].email;
+		const hash = await db('login').where({ email }).select('password');
+		const isValid = bcrypt.compareSync(oldPassword, hash[0].password);
+
+		if (isValid) {
+			bcrypt.hash(newPassword, 10, async (err, hash) => {
+				if (err) throw err;
+
+				try {
+					await db('login').where({ email }).update({ password: hash });
+					res.json('Password Updated');
+				} catch {
+					res
+						.status(400)
+						.json(
+							'Password must contain:\n  • Between 8 and 15 characters\n  • At least 1 lowercase letter\n  • At least 1 uppercase letter\n  • At least 1 numeric digit\n  • At least 1 special character'
+						);
+				}
+			});
+		} else {
+			res.status(400).json('Uh oh! Something went wrong.');
+		}
 	}
 };
 
@@ -64,5 +114,6 @@ const validatePassword = password => {
 };
 
 module.exports = {
-	registerUser
+	registerUser,
+	updateUserProfile
 };
